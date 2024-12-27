e2b944657128efb0b9bff32a60106a036b29304e
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2024-12-28 00:23:49 +0700

    feat: add functionalities to format GitHub activity messages
    - Implement several functions in functions.php to handle specific GitHub event types.
    - Integrate these functions to github-activity.php to display the fetched activity in the terminal.
    - Dynamically generate messages based on event payload data.

--- functions.php
+++ functions.php
@@ -30,3 +30,145 @@ function getGithubEvents(string $username): string|array
 
   return $result;
 }
+
+function repoName(array $event): string
+{
+  return $event['repo']['name'];
+}
+
+function commitCommentEvent(array $event): string
+{
+  $action = $event['payload']['action'];
+  $actor = $event['actor']['login'];
+  $commit_id = $event['payload']['comment']['commit_id'];
+
+  return "Commit comment $action by $actor on commit '$commit_id' in " . repoName($event);
+}
+
+function createEvent(array $event): string
+{
+  $ref = $event['payload']['ref'];
+  $refType = $event['payload']['ref_type'];
+  $payloadRefType = $refType == 'repository'
+    ? $refType
+    : $refType . " [$ref] in";
+
+  return "Created new $payloadRefType " . repoName($event);
+}
+
+function deleteEvent(array $event): string
+{
+  $ref = $event['payload']['ref'];
+  $refType = $event['payload']['ref_type'];
+
+  return "Deleted $refType [$ref] in " . repoName($event);
+}
+
+function forkEvent(array $event): string
+{
+  $forkeeFullName = $event['payload']['forkee']['full_name'];
+
+  return "Forked " . repoName($event) . " to $forkeeFullName";
+}
+
+function gollumEvent(array $event): string
+{
+  $action = ucfirst($event['payload']['pages'][0]['action']);
+
+  return "$action a wiki page in " . repoName($event);
+}
+
+function issueCommentEvent(array $event): string
+{
+  $action = ucfirst($event['payload']['action']);
+  $issueNumber = $event['payload']['issue']['number'];
+
+  return "$action comment on issue #$issueNumber in " . repoName($event);
+}
+
+function issueEvent(array $event): string
+{
+  $action = ucfirst($event['payload']['action']);
+  $payloadAction = $action == 'opened'
+    ? $action . ' a new issue'
+    : $action . ' an issue';
+
+  return "$payloadAction in " . repoName($event);
+}
+
+function memberEvent(array $event): string
+{
+  $action = ucfirst($event['payload']['action']);
+  $member = $event['payload']['member']['login'];
+  $message = $action == 'Added'
+    ? 'as a collaborator to'
+    : ($action == 'Edited'
+      ? 'permissions as a collaborator in'
+      : 'from as a collaborator from');
+
+  return "$action $member $message " . repoName($event);
+}
+
+function publicEvent(array $event): string
+{
+  return "Changed visibility to public for " . repoName($event);
+}
+
+function pullRequestEvent(array $event): string
+{
+  $action = $event['payload']['action'];
+  $number = $event['payload']['number'];
+  $payloadAction = strpos($action, '_') != false
+    ? ucfirst(str_replace('_', ' ', $action))
+    : ucfirst($action);
+
+  return "$payloadAction pull request #$number in " . repoName($event);
+}
+
+function pullRequestReviewEvent(array $event): string
+{
+  $actor = $event['actor']['login'];
+  $action = $event['payload']['action'];
+  $number = $event['payload']['pull_request']['number'];
+  $opening = strpos($event['type'], 'Comment') !== false
+    ? 'Review comment'
+    : (strpos($event['type'], 'Thread') !== false
+      ? 'Review thread'
+      : 'Review');
+
+  return "$opening $action by $actor for pull request #$number in " . repoName($event);
+}
+
+function pushEvent(array $event): string
+{
+  $size = $event['payload']['size'];
+  $payloadSize = $size > 1
+    ? $size . ' commits'
+    : $size . ' commit';
+
+  return "Pushed $payloadSize to " . repoName($event);
+}
+
+function releaseEvent(array $event): string
+{
+  $action = ucfirst($event['payload']['action']);
+  $tagName = $event['payload']['release']['tag_name'];
+  $release = $action == 'Published'
+    ? 'new release'
+    : 'release';
+
+  return "$action $release $tagName in " . repoName($event);
+}
+
+function sponsorshipEvent(array $event): string
+{
+  $action = ucfirst($event['payload']['action']);
+  $actor = $event['actor']['login'];
+
+  return "$action sponsorship by $actor in " . repoName($event);
+}
+
+function watchEvent(array $event): string
+{
+  return "Starred " . repoName($event);
+}
--- github-activity.php
+++ github-activity.php
@@ -4,5 +4,78 @@ require_once 'functions.php';
 
 if (isset($argv[1])) {
   $events = getGithubEvents($argv[1]);
-  var_dump($events);
+
+  if (is_array($events)) {
+    $output = ["Output:"];
+
+    foreach ($events as $event) {
+      switch ($event['type']) {
+        case 'CommitCommentEvent':
+          array_push($output, commitCommentEvent($event));
+          break;
+
+        case 'CreateEvent':
+          array_push($output, createEvent($event));
+          break;
+
+        case 'DeleteEvent':
+          array_push($output, deleteEvent($event));
+          break;
+
+        case 'ForkEvent':
+          array_push($output, forkEvent($event));
+          break;
+
+        case 'GollumEvent':
+          array_push($output, gollumEvent($event));
+          break;
+
+        case 'IssueCommentEvent':
+          array_push($output, issueCommentEvent($event));
+          break;
+
+        case 'IssueEvent':
+          array_push($output, issueEvent($event));
+          break;
+
+        case 'MemberEvent':
+          array_push($output, memberEvent($event));
+          break;
+
+        case 'PublicEvent':
+          array_push($output, publicEvent($event));
+          break;
+
+        case 'PullRequestEvent':
+          array_push($output, pullRequestEvent($event));
+          break;
+
+        case strpos($event['type'], 'PullRequestReview') !== false:
+          array_push($output, pullRequestReviewEvent($event));
+          break;
+
+        case 'PushEvent':
+          array_push($output, pushEvent($event));
+          break;
+
+        case 'ReleaseEvent':
+          array_push($output, releaseEvent($event));
+          break;
+
+        case 'SponsorshipEvent':
+          array_push($output, sponsorshipEvent($event));
+          break;
+
+        case 'WatchEvent':
+          array_push($output, watchEvent($event));
+          break;
+      }
+    }
+
+    $output[count($output) - 1] .= "\n";
+    echo implode("\n- ", $output);
+    exit;
+  }
+
+  echo $events;
 }
